632009ba110713ca8599ea1fc7d41dcf45d59892
Bilal <benarbia0204@gmail.com>
2026-06-19 14:48:21 +0200

    chore: remove next-intl plugin

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  turbopack: {
-    resolveAlias: {
-      'next-intl/config': './i18n/request.ts',
-    },
-  },
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
